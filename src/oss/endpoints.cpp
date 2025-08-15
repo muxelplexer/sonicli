935967ebfed1dfde791c6ec06eb2712b8a9d462e
Sebastian Muxel <sebastian@muxel.dev>
2025-08-16 00:37:26 +0200

    ui: hot-request tracks
    Signed-off-by: Sebastian Muxel <sebastian@muxel.dev>

--- src/oss/endpoints.cpp
+++ src/oss/endpoints.cpp
@@ -4,8 +4,12 @@
 #include "cpr/session.h"
 #include "oss/data/subsonic_response.hpp"
 #include <algorithm>
+#include <chrono>
 #include <cpr/cpr.h>
+#include <cpr/response.h>
+#include <cpr/timeout.h>
 #include <iostream>
+#include <iterator>
 import nlohmann.json;
 
 namespace oss
@@ -41,7 +45,7 @@ namespace oss
     std::optional<data::album_list_response> getAlbumList(const server_config& config)
     {
         auto params { *config.parameters };
-        params.Add({ { "type", "newest" }, { "size", "25" } });
+        params.Add({ { "type", "newest" }, { "size", "500" } });
         const auto res { cpr::Get(cpr::Url(config.url_string + "/rest/getAlbumList.view"), params) };
         if (res.error)
         {
@@ -93,7 +97,30 @@ namespace oss
             multi.AddSession(session);
         }
 
-        const auto responses { multi.Get() };
+        auto time2{std::chrono::steady_clock::now()};
+
+        std::shared_ptr<cpr::Session> session = std::make_shared<cpr::Session>();
+        std::vector<cpr::AsyncResponse> async_responses{};
+        std::ranges::for_each(albums_list,
+                [&async_responses, &uri, &config](const auto& album){
+                    auto params { *config.parameters };
+                    params.Add({
+                        { "id", album.id },
+                    });
+                    static constexpr int32_t timeout_ms{500};
+                    async_responses.emplace_back(cpr::GetAsync(uri, params, cpr::Timeout{timeout_ms}));
+                }
+        );
+        std::vector<cpr::Response> responses{};
+        std::ranges::transform(async_responses, std::back_inserter(responses), [](auto& album){
+            return album.get();
+        });
+
+        auto time2e{std::chrono::steady_clock::now()};
+        std::chrono::duration<double> diff2{time2e - time2};
+        std::cerr << std::format("Took2: {}s\n", diff2.count());
+
+
         std::vector<data::album_response> song_responses {};
         std::ranges::transform(std::views::filter(responses,
                                                   [](const auto& res)
