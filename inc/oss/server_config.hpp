3cc60b9723562d836e3b53ef55d7fdfd770707fa
Sebastian Muxel <sebastian@muxel.dev>
2025-08-16 00:34:36 +0200

    oss: server_config: implement basic TOML initialization
    Signed-off-by: Sebastian Muxel <sebastian@muxel.dev>

--- inc/oss/server_config.hpp
+++ inc/oss/server_config.hpp
@@ -15,11 +15,15 @@ namespace oss
     struct server_config
     {
         server_config(std::string user, std::string url);
+        server_config(std::string&& user, std::string&& url, std::unique_ptr<crypto::password>&& password);
         std::optional<std::string> login();
 
         std::string url_string;
         std::string user;
         std::unique_ptr<crypto::password> password { nullptr };
         std::unique_ptr<cpr::Parameters> parameters { std::make_unique<cpr::Parameters>() };
+
+        static std::optional<server_config> from_file(const std::string_view path);
+        static std::optional<server_config> from_file();
     };
 } // namespace oss
--- src/oss/server_config.cpp
+++ src/oss/server_config.cpp
@@ -1,11 +1,20 @@
 #include "oss/server_config.hpp"
 #include "oss/endpoints.hpp"
+#include <exception>
+#include <filesystem>
 #include <format>
+#include <fstream>
+#include <iostream>
 #include <memory>
 #include <optional>
+#include <stdexcept>
+#include <string_view>
 #include <utility>
 
+#include "util/xdg.hpp"
+
 import nlohmann.json;
+import tomlplusplus;
 
 namespace oss
 {
@@ -13,6 +22,17 @@ namespace oss
         : url_string { std::move(url) }
         , user { std::move(user) }
     {
+    }
+
+    server_config::server_config(std::string&& user, std::string&& url, std::unique_ptr<crypto::password>&& password)
+        : url_string{std::move(url)}
+        , user{std::move(user)}
+        , password{std::move(password)}
+    {
+        if (auto log{this->login()}; log)
+        {
+            throw std::runtime_error(*log);
+        }
     }
 
     std::optional<std::string> server_config::login()
@@ -48,5 +68,50 @@ namespace oss
         }
 
         return std::nullopt;
+    }
+
+    std::optional<server_config> server_config::from_file(const std::string_view path) try
+    {
+        using namespace std::string_literals;
+        std::cerr << "Config path: " << path << "\n";
+        const auto toml_file{toml::parse_file(path)};
+
+        auto username{toml_file["user"]["username"].value_or(""s)};
+        auto url{toml_file["server"]["url"].value_or(""s)};
+        if (auto password_val{toml_file["user"]["password"]}; password_val)
+        {
+            auto password_str{password_val.value<std::string>()};
+            std::unique_ptr<crypto::password> password{std::make_unique<crypto::password>(std::string_view{*password_str})};
+            return std::make_optional<server_config>(std::move(username), std::move(url), std::move(password));
+        }
+
+        return std::make_optional<server_config>(username, url);
+    } catch (const std::exception& ex)
+    {
+        std::cerr << ex.what() << "\n";
+        return std::nullopt;
+    }
+
+    std::optional<server_config> server_config::from_file()
+    {
+        std::filesystem::path config_path{util::xdg_config_home().value_or(util::xdg_config_default())};
+        config_path /= "sonicli";
+
+        std::cerr << "Config path: " << config_path << "\n";
+
+        if (!std::filesystem::exists(config_path))
+        {
+            std::cerr << "Creating directory" << "\n";
+            std::filesystem::create_directory(config_path);
+        }
+
+        config_path /= "config.toml";
+        if (!std::filesystem::exists(config_path))
+        {
+            std::cerr << "No config" << "\n";
+            return std::nullopt;
+        }
+        const auto path_str{config_path.string()};
+        return from_file(path_str);
     }
 } // namespace oss
