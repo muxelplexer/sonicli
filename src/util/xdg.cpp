6dab1b4f45c4951521b35ed0e65f93424dcbe59c
Sebastian Muxel <sebastian@muxel.dev>
2025-08-16 00:33:49 +0200

    util: implement convenient functions for XDG base dir
    Signed-off-by: Sebastian Muxel <sebastian@muxel.dev>

---
+++ src/util/xdg.cpp
@@ -0,0 +1,18 @@
+#include "util/xdg.hpp"
+#include <cstdlib>
+#include <optional>
+#include <string>
+
+namespace util
+{
+    std::string xdg_config_default()
+    {
+        std::string home{::getenv("HOME")};
+        return home + "/.config";
+    }
+    std::optional<std::string> xdg_config_home()
+    {
+        auto* home{::getenv("XDG_CONFIG_HOME")};
+        return (home != nullptr) ? std::make_optional(home) : std::nullopt;
+    }
+}
